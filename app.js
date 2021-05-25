949959938e3e261d062f6160d1297a145dc9ab7e
Abhishek Pragada <abhi.venkata54@gmail.com>
2021-05-25 16:24:06 +0530

    EJS

--- app.js
+++ app.js
@@ -2,24 +2,40 @@ const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
 
+const temp = "";
+var iconurl;
+const des = "";
+const options = {
+    day : "numeric",
+    month : "short",
+    weekday : "short",
+    };
+const date = new Date().toLocaleDateString('en-US',options);
+
 const app = express();
+app.set('view engine', 'ejs');
 
-/* app.set('view engine', 'ejs'); */
-
-app.use(express.static("style"));
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 
-    res.sendFile(__dirname+"/index.html")
+    const query = req.body.cityName
+
+    res.render('index',{
+        des : des,
+        iconurl : iconurl,
+        temp : temp,
+        date : date,
+        query : query
+    });
 
 })
 
 app.post('/', (req, res) => {
 
     const query = req.body.cityName
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ query +'&appid=XXXX&units=metric';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ query +'&appid=XXX&units=metric';
 
     https.get(url, (response) => {
  
@@ -28,30 +44,18 @@ app.post('/', (req, res) => {
            const des = wd.weather[0].description;
            const icon = wd.weather[0].icon;
            const temp = wd.main.temp;
-           const press = wd.main.pressure;
-           const hum = wd.main.humidity;
            const iconurl = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
-           res.write("<body style=\"background-color:#90e0ef;\">")
-           res.write("<p style=\"text-align:center; padding: 50px 0 0 0 ;font-size: 30px; font-family:arial\">The current temperature in "+ query +" is <b>" + temp +" <span>&#176;</span>C</b></p>")
-           res.write("<p style=\"text-align:center; padding: 15px; font-size: 30px;font-family:arial\">Pressure : " + press + " Pa   Humidity : "+ hum +"</p>")
-           res.write("<p style=\"text-align:center; padding: 15px; font-size: 30px;font-family:arial\">The weather is " + des + "</p>")
-           res.write("<div style=\"text-align:center;\">")
-           res.write("<img style=\" left-margin : 500px\" widht=100 height=100 src = "+ iconurl + ">")
-           res.write("</div>")
-           res.write("</body>")
-           /* res.render('ejs/index', {
-               query : query,
-               temp : temp,
+           
+           res.render('index', {
                des : des,
-               iconurl : iconurl
-           }); */
-           res.send()
+               iconurl : iconurl,
+               temp : temp,
+               date : date,
+               query : query
+           });
         })
     })
-
-
 })
-
 
 app.listen(3000, () => {
     console.log("listening on port 3000");
